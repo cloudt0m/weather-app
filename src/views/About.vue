1c5292ac253233d7c3f328f3d47c40dcf3b5f657
tom hsieh <cloudtom@gmail.com>
2021-03-12 15:31:34 +0800

    UPDT about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-4xl py-6">Taiwan Weather App</h1>
+    <h2 class="text-xl py-2">Fetch weather data according to your location.<br />Work with following APIs:<br />
+      <ul class="list-disc py-6 ml-8">
+        <li
+          v-for="(item, index) in techLists"
+          :key="index"
+        >{{ item }}</li>
+      </ul>
+    </h2>
+    <p></p>
   </div>
 </template>
+
+<script lang="ts">
+import { Vue, Options } from "vue-class-component";
+
+@Options({})
+export default class About extends Vue {
+  techLists = [
+    "Browser Geolocation API",
+    "Google Maps Geocoding API",
+    "Central Weather Bureau API",
+  ];
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/style";
+
+.about {
+  background: $clear-bg;
+  font-family: "Oswald", sans-serif;
+  color: $clear-text;
+  @apply w-full h-full flex flex-col justify-center items-center;
+  h1 {
+    color: #ffffff;
+  }
+}
+</style>
+
